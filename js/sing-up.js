3a7a795b407160548aa07f53b8fd52eeb1d9ec43
Oleksandr Hlushach <shoppingplaneta@gmail.com>
2022-09-16 14:28:54 +0300

    add style for verify page

--- js/sing-up.js
+++ js/sing-up.js
@@ -2,13 +2,20 @@ const singUp = document.querySelector('.sing-up-block');
 const banner = document.querySelector('#phone-banner');
 const createProfile = document.querySelector('.create-profile');
 
+const yesBtn = document.querySelector('#yes');
 const nextPage = document.querySelector('.sing-up-next');
+
 const nextBTN = document.querySelector('#next');
 const createProfileModify = document.querySelector('#create-profile');
 
 banner.addEventListener('click', () => {
   singUp.classList.add('sing-up-block_hide');
   createProfile.classList.remove('create-profile_hide');
+});
+
+yesBtn.addEventListener('click', () => {
+  singUp.classList.add('sing-up-block_hide');
+  nextPage.classList.remove('sing-up-next_hide');
 });
 
 nextBTN.addEventListener('click', () => {
