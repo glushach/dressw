874747742b0617675f3404394ca857c0ed924b3b
Oleksandr Hlushach <shoppingplaneta@gmail.com>
2022-09-16 13:47:57 +0300

    add style for sign up modify

--- js/sing-up.js
+++ js/sing-up.js
@@ -2,7 +2,16 @@ const singUp = document.querySelector('.sing-up-block');
 const banner = document.querySelector('#phone-banner');
 const createProfile = document.querySelector('.create-profile');
 
+const nextPage = document.querySelector('.sing-up-next');
+const nextBTN = document.querySelector('#next');
+const createProfileModify = document.querySelector('#create-profile');
+
 banner.addEventListener('click', () => {
   singUp.classList.add('sing-up-block_hide');
   createProfile.classList.remove('create-profile_hide');
+});
+
+nextBTN.addEventListener('click', () => {
+  nextPage.classList.add('create-profile_hide');
+  createProfileModify.classList.remove('create-profile-modify_hide');
 });
